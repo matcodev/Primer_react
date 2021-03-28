0df24afa0a2a4a36863694dd20e9db4d453658de
mxespin <matias@clippercapital.cl>
2021-03-28 19:31:24 -0300

    add function hide modal line 17 Modal.js

--- src/components/CardImages.js
+++ src/components/CardImages.js
@@ -12,29 +12,33 @@ const CardImages = () => {
         urlImage = "https://m.economictimes.com/thumb/height-450,width-600,imgsize-72946,msid-77802791/outlook-2-istock.jpg"
         subTitle =  'Detección del problema'
         paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reprehenderit corrupti sapiente alias eaque quod nostrum eligendi officia rem illo?'
+        modalId =   'modal1'
         modalText = 'Modal 1'
-        textModal = 'Text Modal 1'
+        textModal = 'Texto de relleno para Modal 1'
         />
         <CardImage 
         urlImage = {Executive1}
         subTitle =  'propuesta de valor'
         paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reprehenderit corrupti sapiente alias eaque quod nostrum eligendi officia rem illo?'
+        modalId =   'modal2'
         modalText = 'Modal 2'
-        textModal = 'Text Modal 2'
+        textModal = 'Texto de relleno para Modal 2'
         />
         <CardImage 
         urlImage = "https://www.vippng.com/png/full/105-1059606_handshake-clipart-mutual-agreement-business-partner-clipart-png.png"
         subTitle =  'Compromiso y respuesta'
         paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reprehenderit corrupti sapiente alias eaque quod nostrum eligendi officia rem illo?'
+        modalId =   'modal3'
         modalText = 'Modal 3'
-        textModal = 'Text Modal 3'
+        textModal = 'Texto de relleno para Modal 3'
         />
         <CardImage 
         urlImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9pBAyVLBxHu-XM5Bh4djbcDunQr2DCEibaw&usqp=CAU"
         subTitle =  'Servicio al Cliente 24/7'
         paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reprehenderit corrupti sapiente alias eaque quod nostrum eligendi officia rem illo?'
+        modalId =   'modal1'
         modalText = 'Modal 4'
-        textModal = 'Text Modal 4'
+        textModal = 'Texto de relleno para Modal 4'
         />
       </div>
     </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
 const Modal = (props) => {
-    
-    const {showModal , setShowModal, modalText,textModal} = props;
-    
+
+    const { showModal, setShowModal, modalText, textModal, modalId } = props;
+
+    const hideModal = (e) => {
+        const modal = document.getElementById(e.target.id);
+        if (modal && modal.contains(e.target)) {
+            modal.style.display = "none"
+        }
+    }
+
+   
+
     return (
-        <div className = {showModal === true ? 'modal' : 'none-modal'} >
-             <div className="modalContent">
-                <span className="close" id="closeModal1" onClick={()=>setShowModal(false)}>×</span>
-                <p>{modalText}</p>
-                <p>{textModal}</p>
-              </div>
+        <div id={modalId} className="close" onClick={hideModal}>
+            <div className={showModal === true ? 'modal' : 'none-modal'} >
+                <div className="modalContent">
+                    <span className="close" onClick={() => setShowModal(false)}>×</span>
+                    <p>{modalText}</p>
+                    <p>{textModal}</p>
+                </div>
+            </div>
         </div>
     )
 }
